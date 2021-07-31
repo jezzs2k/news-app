9ac9a172d659fdc53fefced75fbefca0b53f77c4
Vu Thanh Hieu <vuthanhhieu00@gmail.com>
2021-07-31 20:01:39 +0700

    add loading for webview

--- src/components/WebView.js
+++ src/components/WebView.js
@@ -1,6 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ActivityIndicator, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export const MyWebComponent = ({ item }) => {
-    return <WebView source={{ uri: item.detail }} />;
+    const [loading, setLoading] = useState(false);
+
+    return <View style={{ flex: 1, backgroundColor: '#fff' }}>
+        {loading && <ActivityIndicator style={{ paddingVertical: 8 }} color={'#000'} size={'small'} />}
+        <WebView source={{ uri: item.detail }} onLoadEnd={() => {
+            setLoading(false);
+        }} onLoadStart={() => {
+            setLoading(true);
+        }} />
+    </View>
 }
